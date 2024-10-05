0d113649aa4c59cffdb3945a052fa7b40da9592c
Liomlong <529855617@qq.com>
2024-10-05 13:16:02 +0800

    更新1

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,13 @@
-import Redis from 'ioredis'
+// 暂时注释掉 Redis 相关代码
+// import Redis from 'ioredis';
+// const redis = new Redis(process.env.REDIS_URL);
+// export default redis;
 
-const redis = new Redis(process.env.REDIS_URL)
+// 使用一个空对象模拟 Redis 客户端
+const mockRedis = {
+  get: async () => null,
+  set: async () => {},
+  // 添加其他你可能用到的方法
+};
 
-export default redis
+export default mockRedis;
